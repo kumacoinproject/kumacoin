1fdc3bcc88266c29681fbdc62b5346d04aeff45e
eagle <eaglemw@gmail.com>
2015-02-04 22:29:21 +0900

    add scrypt-arm.S

--- src/scrypt_mine.cpp
+++ src/scrypt_mine.cpp
@@ -29,7 +29,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include <xmmintrin.h>
+// #include <xmmintrin.h>
 
 #include "scrypt_mine.h"
 #include "pbkdf2.h"
@@ -54,7 +54,7 @@ extern "C" void scrypt_core(uint32_t *X, uint32_t *V);
 extern "C" void scrypt_core_2way(uint32_t *X, uint32_t *Y, uint32_t *V);
 extern "C" void scrypt_core_3way(uint32_t *X, uint32_t *Y, uint32_t *Z, uint32_t *V);
 
-#elif defined(__i386__)
+#elif defined(__i386__) || defined(__arm__)
 
 #define SCRYPT_BUFFER_SIZE (131072 + 63)
 
